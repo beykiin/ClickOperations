78e411e048851185168dea18b70e381657e4b56e
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 16:18:38 +0300

    First Commit

---
+++ click.js
@@ -0,0 +1,19 @@
+var btn=document.getElementById("btn")
+var box=document.getElementById("box")
+// 1-ONCLİCK İŞLEMİ FONKSİYON ŞEKLİNDE YAZILIR BUTONA BASILDIĞINDA NE OLACAĞINI GÖSTERİR
+// btn.onclick=()=>{
+//     box.style.width="100px"
+//     box.style.height="200px"
+//     box.style.border="7px solid darkblue"
+//     box.style.backgroundColor="yellow"
+//     box.style.borderRadius="5px"
+// }
+
+// 2-ADDEVENTLİSTENER İSE DİNLEME İŞLEMİ YİNE FONKSİYON FAKAT İLK KISMA HANGİ İŞLEM GERÇEKLEŞTİĞİNDE İSTEDİĞİNİ YAZICAKSIN VE SONRA FONKSİYON AÇIP İÇİNE GERÇEKLEŞECEK OLAY YAZILACAK "Keyup","keydown","click" yani tuştan elini kaldırdığın tuşa bastığında yada click işlemi gerçekleştirdiğinde gibi
+// btn.addEventListener("click",()=>{
+//     box.style.width="100px"
+//     box.style.height="200px"
+//     box.style.border="7px solid darkblue"
+//     box.style.backgroundColor="yellow"
+//     box.style.borderRadius="5px"
+// })
